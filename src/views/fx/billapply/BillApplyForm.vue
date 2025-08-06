a5708627f4d7e99577fdee10a21bf0a5bff77ef5
tanglilong <tanglilong@puqi.group>
2025-08-06 11:07:48 +0800

    优化代码

--- src/views/fx/billapply/BillApplyForm.vue
+++ src/views/fx/billapply/BillApplyForm.vue
@@ -9,166 +9,205 @@
       v-loading="formLoading"
       class="bill-apply-form"
     >
-      <!-- 申请人 -->
-      <el-form-item label="申请人" prop="applyMan">
-        <el-select v-model="formData.applyMan" multiple placeholder="请选择申请人" :disabled="true">
-          <el-option
-            v-for="item in userOptions"
-            :key="item.id"
-            :label="item.nickname"
-            :value="item.id"
-          />
-        </el-select>
-      </el-form-item>
-      <!-- 申请日期 -->
-      <el-form-item label="申请日期" prop="applyDate">
-        <el-date-picker
-          v-model="formData.applyDate"
-          type="date"
-          value-format="x"
-          placeholder="选择申请日期"
-        />
-      </el-form-item>
-      <!-- 开票日期 -->
-      <el-form-item label="开票日期" prop="billDate">
-        <el-date-picker
-          v-model="formData.billDate"
-          type="date"
-          value-format="x"
-          placeholder="选择开票日期"
-        />
-      </el-form-item>
-      <!-- 地址及电话 -->
-      <el-form-item label="地址及电话" prop="address">
-        <el-input v-model="formData.address" placeholder="请输入地址及电话" />
-      </el-form-item>
-      <!-- 开户行及账号 -->
-      <el-form-item label="开户行及账号" prop="bankNo">
-        <el-input v-model="formData.bankNo" placeholder="请输入开户行及账号" />
-      </el-form-item>
-      <!-- 金额合计 -->
-      <el-form-item label="金额合计" prop="amount">
-        <el-input v-model="formData.amount" placeholder="请输入金额合计" />
-      </el-form-item>
-      <!-- 备注 -->
-      <el-form-item label="备注" prop="remark">
-        <el-input v-model="formData.remark" placeholder="请输入备注" type="textarea" rows="3" />
-      </el-form-item>
-      <!-- 发票类型 -->
-      <el-form-item label="发票类型" prop="billType">
-        <el-select v-model="formData.billType" placeholder="请选择发票类型">
-          <el-option
-            v-for="dict in getDictOptions(DICT_TYPE.FX_BILL_TYPE)"
-            :key="dict.value"
-            :label="dict.label"
-            :value="dict.value"
-          />
-        </el-select>
-      </el-form-item>
-      <!-- 金额合计(大写) -->
-      <el-form-item label="金额合计(大写)" prop="totalAmount">
-        <el-input v-model="formData.totalAmount" placeholder="请输入金额合计(大写)" />
-      </el-form-item>
-      <!-- 发票抬头 -->
-      <el-form-item label="发票抬头" prop="billHead">
-        <el-select v-model="formData.billHead" placeholder="请选择发票抬头">
-          <el-option
-            v-for="dict in getStrDictOptions(DICT_TYPE.FX_BUSINESS_ENTITY)"
-            :key="dict.value"
-            :label="dict.label"
-            :value="dict.value"
-          />
-        </el-select>
-      </el-form-item>
-      <!-- 发票附件 -->
-      <el-form-item label="发票附件" prop="document">
-        <el-upload
-          class="upload-demo"
-          action="/api/file/upload"
-          :on-success="handleFileSuccess"
-          :file-list="fileList"
-          list-type="text"
-        >
-          <el-button type="primary">点击上传</el-button>
-          <template #tip>
-            <div class="el-upload__tip">支持上传pdf、jpg、png格式文件，单个文件不超过10MB</div>
-          </template>
-        </el-upload>
-      </el-form-item>
-      <!-- 财务说明 -->
-      <el-form-item label="财务说明" prop="financialStatement">
-        <el-input
-          v-model="formData.financialStatement"
-          placeholder="请输入财务说明"
-          type="textarea"
-          rows="3"
-        />
-      </el-form-item>
-      <!-- 购方名称 -->
-      <el-form-item label="购方名称" prop="purchaserName">
-        <el-input v-model="formData.purchaserName" placeholder="请输入购方名称" />
-      </el-form-item>
-      <!-- 纳税人识别号 -->
-      <el-form-item label="纳税人识别号" prop="taxNo">
-        <el-input v-model="formData.taxNo" placeholder="请输入纳税人识别号" />
-      </el-form-item>
-      <!-- 开票流程 -->
-      <el-form-item label="开票流程" prop="rid">
-        <el-input v-model="formData.rid" placeholder="请输入开票流程" />
-      </el-form-item>
-      <!-- 是否完成 -->
-      <el-form-item label="是否完成" prop="isOver">
-        <el-select v-model="formData.isOver" placeholder="请选择是否完成">
-          <el-option
-            v-for="dict in getDictOptions(DICT_TYPE.YES_NO)"
-            :key="dict.value"
-            :label="dict.label"
-            :value="dict.value"
-          />
-        </el-select>
-      </el-form-item>
-      <!-- 是否芽肌 -->
-      <el-form-item label="是否芽肌" prop="isYj">
-        <el-select v-model="formData.isYj" placeholder="请选择是否芽肌">
-          <el-option
-            v-for="dict in getDictOptions(DICT_TYPE.YES_NO)"
-            :key="dict.value"
-            :label="dict.label"
-            :value="dict.value"
-          />
-        </el-select>
-      </el-form-item>
-      <!-- 发票邮箱 -->
-      <el-form-item label="发票邮箱" prop="email">
-        <el-input v-model="formData.email" placeholder="请输入发票邮箱" />
-      </el-form-item>
-      <!-- 发票发送状态 -->
-      <el-form-item label="发票发送状态" prop="isSend">
-        <el-select v-model="formData.isSend" placeholder="请选择发送状态">
-          <el-option label="未发送" value="0" />
-          <el-option label="已发送" value="1" />
-        </el-select>
-      </el-form-item>
-    </el-form>
+      <!-- 表单分组：基础信息 -->
+      <div class="form-section">
+        <h3 class="section-title">基础信息</h3>
+        <el-row :gutter="20">
+          <el-col :span="12">
+            <el-form-item label="申请人" prop="applyMan">
+              <el-select v-model="formData.applyMan" multiple placeholder="请选择申请人" :disabled="true">
+                <el-option
+                  v-for="item in userOptions"
+                  :key="item.id"
+                  :label="item.nickname"
+                  :value="item.id"
+                />
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="申请日期" prop="applyDate">
+              <el-date-picker
+                v-model="formData.applyDate"
+                type="date"
+                value-format="x"
+                placeholder="选择申请日期"
+              />
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="开票日期" prop="billDate">
+              <el-date-picker
+                v-model="formData.billDate"
+                type="date"
+                value-format="x"
+                placeholder="选择开票日期"
+              />
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="发票类型" prop="billType">
+              <el-select v-model="formData.billType" placeholder="请选择发票类型">
+                <el-option
+                  v-for="dict in getIntDictOptions(DICT_TYPE.FX_BILL_TYPE)"
+                  :key="dict.value"
+                  :label="dict.label"
+                  :value="dict.value"
+                />
+              </el-select>
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </div>
 
-    <!-- 子表的表单 -->
-    <el-tabs v-model="subTabsName" class="bill-sub-tabs">
-      <el-tab-pane label="发票申请详情" name="billApplyDetail">
-        <BillApplyDetailForm ref="billApplyDetailFormRef" :main-id="formData.id" />
-      </el-tab-pane>
-    </el-tabs>
+      <!-- 表单分组：发票信息 -->
+      <div class="form-section">
+        <h3 class="section-title">发票信息</h3>
+        <el-row :gutter="20">
+          <el-col :span="12">
+            <el-form-item label="发票抬头" prop="billHead">
+              <el-select v-model="formData.billHead" placeholder="请选择发票抬头">
+                <el-option
+                  v-for="dict in getStrDictOptions(DICT_TYPE.FX_BUSINESS_ENTITY)"
+                  :key="dict.value"
+                  :label="dict.label"
+                  :value="dict.value"
+                />
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="金额合计" prop="amount">
+              <el-input v-model="formData.amount" placeholder="请输入金额合计" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="金额合计(大写)" prop="totalAmount">
+              <el-input v-model="formData.totalAmount" placeholder="请输入金额合计(大写)" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="开票流程" prop="rid">
+              <el-input v-model="formData.rid" placeholder="请输入开票流程" />
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </div>
+
+      <!-- 表单分组：购方信息 -->
+      <div class="form-section">
+        <h3 class="section-title">购方信息</h3>
+        <el-row :gutter="20">
+          <el-col :span="12">
+            <el-form-item label="购方名称" prop="purchaserName">
+              <el-input v-model="formData.purchaserName" placeholder="请输入购方名称" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="纳税人识别号" prop="taxNo">
+              <el-input v-model="formData.taxNo" placeholder="请输入纳税人识别号" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="24">
+            <el-form-item label="地址及电话" prop="address">
+              <el-input v-model="formData.address" placeholder="请输入地址及电话" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="24">
+            <el-form-item label="开户行及账号" prop="bankNo">
+              <el-input v-model="formData.bankNo" placeholder="请输入开户行及账号" />
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </div>
+
+      <!-- 表单分组：其他信息 -->
+      <div class="form-section">
+        <h3 class="section-title">其他信息</h3>
+        <el-row :gutter="20">
+          <el-col :span="12">
+            <el-form-item label="是否完成" prop="isOver">
+              <el-select v-model="formData.isOver" placeholder="请选择是否完成">
+                <el-option
+                  v-for="dict in getIntDictOptions(DICT_TYPE.YES_NO)"
+                  :key="dict.value"
+                  :label="dict.label"
+                  :value="dict.value"
+                />
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="是否芽肌" prop="isYj">
+              <el-select v-model="formData.isYj" placeholder="请选择是否芽肌">
+                <el-option
+                  v-for="dict in getIntDictOptions(DICT_TYPE.YES_NO)"
+                  :key="dict.value"
+                  :label="dict.label"
+                  :value="dict.value"
+                />
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="发票邮箱" prop="email">
+              <el-input v-model="formData.email" placeholder="请输入发票邮箱" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="发票发送状态" prop="isSend">
+              <el-select v-model="formData.isSend" placeholder="请选择发送状态">
+                <el-option label="未发送" value="0" />
+                <el-option label="已发送" value="1" />
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="24">
+            <el-form-item label="备注" prop="remark">
+              <el-input v-model="formData.remark" placeholder="请输入备注" type="textarea" rows="3" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="24">
+            <el-form-item label="财务说明" prop="financialStatement">
+              <el-input
+                v-model="formData.financialStatement"
+                placeholder="请输入财务说明"
+                type="textarea"
+                rows="3"
+              />
+            </el-form-item>
+          </el-col>
+          <el-col :span="24">
+            <el-form-item label="发票附件" prop="document">
+              <el-upload
+                class="upload-demo"
+                action="/api/file/upload"
+                :on-success="handleFileSuccess"
+                :file-list="fileList"
+                list-type="text"
+              >
+                <el-button type="primary">点击上传</el-button>
+                <template #tip>
+                  <div class="el-upload__tip">支持上传pdf、jpg、png格式文件，单个文件不超过10MB</div>
+                </template>
+              </el-upload>
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </div>
+    </el-form>
 
     <template #footer>
       <el-button @click="dialogVisible = false" class="cancel-btn">取 消</el-button>
       <el-button @click="submitForm" type="primary" :disabled="formLoading" class="confirm-btn"
-        >确 定</el-button
+      >确 定</el-button
       >
     </template>
   </Dialog>
 </template>
 
 <script setup lang="ts">
-import { DICT_TYPE, getDictOptions, getStrDictOptions } from '@/utils/dict'
+import {DICT_TYPE, getDictOptions, getIntDictOptions, getStrDictOptions} from '@/utils/dict'
 import { BillApplyApi, BillApplyVO } from '@/api/fx/billapply'
 import BillApplyDetailForm from './components/BillApplyDetailForm.vue'
 import * as UserApi from '@/api/system/user'
@@ -385,12 +424,42 @@ const resetForm = () => {
 <style scoped>
 /* 弹窗样式优化 */
 .bill-apply-dialog {
-  --el-dialog-width: 800px;
+  --el-dialog-width: 900px;
+  --el-dialog-padding: 20px;
 }
 
 /* 表单样式优化 */
 .bill-apply-form {
-  padding: 15px 0;
+  padding: 10px 0;
+}
+
+/* 表单分组样式 */
+.form-section {
+  margin-bottom: 25px;
+  padding: 15px;
+  background-color: #f9fafb;
+  border-radius: 6px;
+  border: 1px solid #eee;
+}
+
+.section-title {
+  margin: 0 0 15px 0;
+  padding-bottom: 8px;
+  font-size: 16px;
+  color: #1f2329;
+  border-bottom: 1px solid #e5e7eb;
+  display: flex;
+  align-items: center;
+}
+
+.section-title::before {
+  content: '';
+  display: inline-block;
+  width: 4px;
+  height: 16px;
+  background-color: #409eff;
+  margin-right: 8px;
+  border-radius: 2px;
 }
 
 .el-form-item {
@@ -407,22 +476,21 @@ const resetForm = () => {
   resize: vertical;
 }
 
-/* 子表标签页样式 */
-.bill-sub-tabs {
-  margin-top: 15px;
-  border: 1px solid #e5e7eb;
-  border-radius: 4px;
-}
-
 /* 按钮样式优化 */
 :deep(.el-dialog__footer) {
   padding: 15px 20px;
   border-top: 1px solid #e5e7eb;
   margin-top: 10px;
+  display: flex;
+  justify-content: flex-end;
+}
+
+.cancel-btn {
+  margin-right: 10px;
 }
 
 .confirm-btn {
-  margin-left: 10px;
+  margin-left: 0;
 }
 
 /* 上传组件样式 */
@@ -435,11 +503,25 @@ const resetForm = () => {
 /* 响应式调整 */
 @media (max-width: 768px) {
   .bill-apply-dialog {
-    --el-dialog-width: 90%;
+    --el-dialog-width: 95%;
+  }
+
+  .form-section {
+    padding: 10px;
+    margin-bottom: 15px;
+  }
+
+  .section-title {
+    font-size: 14px;
+    margin-bottom: 10px;
   }
 
   .el-form-item {
     margin-bottom: 12px;
+  }
+
+  .el-col {
+    --el-col-span: 24 !important;
   }
 }
 </style>
--- src/views/fx/billapply/index.vue
+++ src/views/fx/billapply/index.vue
@@ -8,35 +8,9 @@
       :inline="true"
       label-width="68px"
     >
-<!--      <el-form-item label="申请人" prop="applyMan">-->
-<!--        <el-select-->
-<!--          v-model="queryParams.applyMan"-->
-<!--          placeholder="请选择申请人"-->
-<!--          clearable-->
-<!--          class="!w-240px"-->
-<!--        >-->
-<!--          <el-option-->
-<!--            v-for="dict in getDictOptions(DICT_TYPE.$dictType.toUpperCase())"-->
-<!--            :key="dict.value"-->
-<!--            :label="dict.label"-->
-<!--            :value="dict.value"-->
-<!--          />-->
-<!--        </el-select>-->
-<!--      </el-form-item>-->
-<!--      <el-form-item label="申请日期" prop="applyDate">-->
-<!--        <el-date-picker-->
-<!--          v-model="queryParams.applyDate"-->
-<!--          value-format="YYYY-MM-DD HH:mm:ss"-->
-<!--          type="daterange"-->
-<!--          start-placeholder="开始日期"-->
-<!--          end-placeholder="结束日期"-->
-<!--          :default-time="[new Date('1 00:00:00'), new Date('1 23:59:59')]"-->
-<!--          class="!w-240px"-->
-<!--        />-->
-<!--      </el-form-item>-->
-      <el-form-item label="开票日期" prop="billDate">
+      <el-form-item label="申请日期" prop="applyDate">
         <el-date-picker
-          v-model="queryParams.billDate"
+          v-model="queryParams.applyDate"
           value-format="YYYY-MM-DD HH:mm:ss"
           type="daterange"
           start-placeholder="开始日期"
@@ -45,57 +19,24 @@
           class="!w-240px"
         />
       </el-form-item>
-<!--      <el-form-item label="地址及电话" prop="address">-->
-<!--        <el-input-->
-<!--          v-model="queryParams.address"-->
-<!--          placeholder="请输入地址及电话"-->
-<!--          clearable-->
-<!--          @keyup.enter="handleQuery"-->
-<!--          class="!w-240px"-->
-<!--        />-->
-<!--      </el-form-item>-->
-<!--      <el-form-item label="开户行及账号" prop="bankNo">-->
-<!--        <el-input-->
-<!--          v-model="queryParams.bankNo"-->
-<!--          placeholder="请输入开户行及账号"-->
-<!--          clearable-->
-<!--          @keyup.enter="handleQuery"-->
-<!--          class="!w-240px"-->
-<!--        />-->
-<!--      </el-form-item>-->
-<!--      <el-form-item label="金额合计" prop="amount">-->
-<!--        <el-input-->
-<!--          v-model="queryParams.amount"-->
-<!--          placeholder="请输入金额合计"-->
-<!--          clearable-->
-<!--          @keyup.enter="handleQuery"-->
-<!--          class="!w-240px"-->
-<!--        />-->
-<!--      </el-form-item>-->
-<!--      <el-form-item label="操作员" prop="maker">-->
-<!--        <el-select-->
-<!--          v-model="queryParams.maker"-->
-<!--          placeholder="请选择操作员"-->
-<!--          clearable-->
-<!--          class="!w-240px"-->
-<!--        >-->
-<!--          <el-option-->
-<!--            v-for="dict in getDictOptions(DICT_TYPE.$dictType.toUpperCase())"-->
-<!--            :key="dict.value"-->
-<!--            :label="dict.label"-->
-<!--            :value="dict.value"-->
-<!--          />-->
-<!--        </el-select>-->
-<!--      </el-form-item>-->
-<!--      <el-form-item label="备注" prop="remark">-->
-<!--        <el-input-->
-<!--          v-model="queryParams.remark"-->
-<!--          placeholder="请输入备注"-->
-<!--          clearable-->
-<!--          @keyup.enter="handleQuery"-->
-<!--          class="!w-240px"-->
-<!--        />-->
-<!--      </el-form-item>-->
+      <el-form-item label="地址及电话" prop="address">
+        <el-input
+          v-model="queryParams.address"
+          placeholder="请输入地址及电话"
+          clearable
+          @keyup.enter="handleQuery"
+          class="!w-240px"
+        />
+      </el-form-item>
+      <el-form-item label="开户行及账号" prop="bankNo">
+        <el-input
+          v-model="queryParams.bankNo"
+          placeholder="请输入开户行及账号"
+          clearable
+          @keyup.enter="handleQuery"
+          class="!w-240px"
+        />
+      </el-form-item>
       <el-form-item label="发票类型" prop="billType">
         <el-select
           v-model="queryParams.billType"
@@ -111,15 +52,6 @@
           />
         </el-select>
       </el-form-item>
-<!--      <el-form-item label="金额合计(大写)" prop="totalAmount">-->
-<!--        <el-input-->
-<!--          v-model="queryParams.totalAmount"-->
-<!--          placeholder="请输入金额合计(大写)"-->
-<!--          clearable-->
-<!--          @keyup.enter="handleQuery"-->
-<!--          class="!w-240px"-->
-<!--        />-->
-<!--      </el-form-item>-->
       <el-form-item label="发票抬头" prop="billHead">
         <el-select
           v-model="queryParams.billHead"
@@ -135,24 +67,6 @@
           />
         </el-select>
       </el-form-item>
-<!--      <el-form-item label="发票附件" prop="document">-->
-<!--        <el-input-->
-<!--          v-model="queryParams.document"-->
-<!--          placeholder="请输入发票附件"-->
-<!--          clearable-->
-<!--          @keyup.enter="handleQuery"-->
-<!--          class="!w-240px"-->
-<!--        />-->
-<!--      </el-form-item>-->
-<!--      <el-form-item label="财务说明" prop="financialStatement">-->
-<!--        <el-input-->
-<!--          v-model="queryParams.financialStatement"-->
-<!--          placeholder="请输入财务说明"-->
-<!--          clearable-->
-<!--          @keyup.enter="handleQuery"-->
-<!--          class="!w-240px"-->
-<!--        />-->
-<!--      </el-form-item>-->
       <el-form-item label="购方名称" prop="purchaserName">
         <el-input
           v-model="queryParams.purchaserName"
@@ -171,108 +85,6 @@
           class="!w-240px"
         />
       </el-form-item>
-<!--      <el-form-item label="客户名称" prop="cusName">-->
-<!--        <el-select-->
-<!--          v-model="queryParams.cusName"-->
-<!--          placeholder="请选择客户名称"-->
-<!--          clearable-->
-<!--          class="!w-240px"-->
-<!--        >-->
-<!--          <el-option-->
-<!--            v-for="dict in getStrDictOptions(DICT_TYPE.$dictType.toUpperCase())"-->
-<!--            :key="dict.value"-->
-<!--            :label="dict.label"-->
-<!--            :value="dict.value"-->
-<!--          />-->
-<!--        </el-select>-->
-<!--      </el-form-item>-->
-<!--      <el-form-item label="开票信息" prop="billInfo">-->
-<!--        <el-select-->
-<!--          v-model="queryParams.billInfo"-->
-<!--          placeholder="请选择开票信息"-->
-<!--          clearable-->
-<!--          class="!w-240px"-->
-<!--        >-->
-<!--          <el-option-->
-<!--            v-for="dict in getStrDictOptions(DICT_TYPE.$dictType.toUpperCase())"-->
-<!--            :key="dict.value"-->
-<!--            :label="dict.label"-->
-<!--            :value="dict.value"-->
-<!--          />-->
-<!--        </el-select>-->
-<!--      </el-form-item>-->
-      <el-form-item label="开票流程" prop="rid">
-        <el-input
-          v-model="queryParams.rid"
-          placeholder="请输入开票流程"
-          clearable
-          @keyup.enter="handleQuery"
-          class="!w-240px"
-        />
-      </el-form-item>
-<!--      <el-form-item label="是否完成" prop="isOver">-->
-<!--        <el-select-->
-<!--          v-model="queryParams.isOver"-->
-<!--          placeholder="请选择是否完成"-->
-<!--          clearable-->
-<!--          class="!w-240px"-->
-<!--        >-->
-<!--          <el-option-->
-<!--            v-for="dict in getDictOptions(DICT_TYPE.YES_NO)"-->
-<!--            :key="dict.value"-->
-<!--            :label="dict.label"-->
-<!--            :value="dict.value"-->
-<!--          />-->
-<!--        </el-select>-->
-<!--      </el-form-item>-->
-<!--      <el-form-item label="是否芽肌" prop="isYj">-->
-<!--        <el-select-->
-<!--          v-model="queryParams.isYj"-->
-<!--          placeholder="请选择是否芽肌"-->
-<!--          clearable-->
-<!--          class="!w-240px"-->
-<!--        >-->
-<!--          <el-option-->
-<!--            v-for="dict in getDictOptions(DICT_TYPE.YES_NO)"-->
-<!--            :key="dict.value"-->
-<!--            :label="dict.label"-->
-<!--            :value="dict.value"-->
-<!--          />-->
-<!--        </el-select>-->
-<!--      </el-form-item>-->
-<!--      <el-form-item label="发票邮箱" prop="email">-->
-<!--        <el-input-->
-<!--          v-model="queryParams.email"-->
-<!--          placeholder="请输入发票邮箱"-->
-<!--          clearable-->
-<!--          @keyup.enter="handleQuery"-->
-<!--          class="!w-240px"-->
-<!--        />-->
-<!--      </el-form-item>-->
-<!--      <el-form-item label="关联邮箱" prop="emailId">-->
-<!--        <el-select-->
-<!--          v-model="queryParams.emailId"-->
-<!--          placeholder="请选择关联邮箱"-->
-<!--          clearable-->
-<!--          class="!w-240px"-->
-<!--        >-->
-<!--          <el-option-->
-<!--            v-for="dict in getStrDictOptions(DICT_TYPE.$dictType.toUpperCase())"-->
-<!--            :key="dict.value"-->
-<!--            :label="dict.label"-->
-<!--            :value="dict.value"-->
-<!--          />-->
-<!--        </el-select>-->
-<!--      </el-form-item>-->
-<!--      <el-form-item label="发票发送状态" prop="isSend">-->
-<!--        <el-input-->
-<!--          v-model="queryParams.isSend"-->
-<!--          placeholder="请输入发票发送状态"-->
-<!--          clearable-->
-<!--          @keyup.enter="handleQuery"-->
-<!--          class="!w-240px"-->
-<!--        />-->
-<!--      </el-form-item>-->
       <el-form-item>
         <el-button @click="handleQuery"><Icon icon="ep:search" class="mr-5px" /> 搜索</el-button>
         <el-button @click="resetQuery"><Icon icon="ep:refresh" class="mr-5px" /> 重置</el-button>
@@ -300,34 +112,25 @@
   <!-- 列表 -->
   <ContentWrap>
     <el-table v-loading="loading" :data="list" :stripe="true" :show-overflow-tooltip="true">
-<!--      <el-table-column label="序号" align="center" prop="id" />-->
       <el-table-column label="申请人" align="center" prop="applyMan" />
       <el-table-column label="申请日期" align="center" prop="applyDate" />
       <el-table-column label="开票日期" align="center" prop="billDate" />
       <el-table-column label="地址及电话" align="center" prop="address" />
       <el-table-column label="开户行及账号" align="center" prop="bankNo" />
       <el-table-column label="金额合计" align="center" prop="amount" />
-      <el-table-column label="操作员" align="center" prop="maker" />
       <el-table-column label="备注" align="center" prop="remark" />
       <el-table-column label="发票类型" align="center" prop="billType">
         <template #default="scope">
           <dict-tag :type="DICT_TYPE.FX_BILL_TYPE" :value="scope.row.billType" />
         </template>
       </el-table-column>
-<!--      <el-table-column label="金额合计(大写)" align="center" prop="totalAmount" />-->
       <el-table-column label="销售单" align="center" prop="saleOrder" />
       <el-table-column label="发票抬头" align="center" prop="billHead">
         <template #default="scope">
           <dict-tag :type="DICT_TYPE.FX_BUSINESS_ENTITY" :value="scope.row.billHead" />
         </template>
       </el-table-column>
-      <el-table-column label="发票附件" align="center" prop="document" />
-      <el-table-column label="财务说明" align="center" prop="financialStatement" />
-      <el-table-column label="购方名称" align="center" prop="purchaserName" />
       <el-table-column label="纳税人识别号" align="center" prop="taxNo" />
-      <el-table-column label="客户名称" align="center" prop="cusName" />
-      <el-table-column label="开票信息" align="center" prop="billInfo" />
-      <el-table-column label="开票流程" align="center" prop="rid" />
 <!--      <el-table-column label="是否完成" align="center" prop="isOver">-->
 <!--        <template #default="scope">-->
 <!--          <dict-tag :type="DICT_TYPE.YES_NO" :value="scope.row.isOver" />-->
